6dc118e4a39e12544a4d7071ee5b714726a5793e
Lourdes Jiménez <00090220@uca.edu.sv>
2025-04-30 00:34:39 -0600

    :iphone: Working on responsive design

--- weatherWeb/src/WeatherWebPage.jsx
+++ weatherWeb/src/WeatherWebPage.jsx
@@ -70,7 +70,7 @@ const WeatherWebPage = () =>{
 
         <div className="text-center font-display bg-WeatherWebPage h-dvh">
             <h1 className="text-4xl font-bold p-8">Weather Web</h1>
-            <div className="flex flex-col place-content-center sm:flex-row sm:justify-center ">
+            <div className="flex flex-col place-self-center sm:flex-row sm:justify-center ">
                 <img src={weatherImage} className="w-36 h-36 place-self-center" alt="Clima del día de hoy" />
                 <div className="grid grid-rows-3 grid-cols-2 gap-2 place-items-start items-center w-2xs m-2 sm:w-xl">
                     <h1 className="col-span-2 text-3xl font-medium place-self-center">{
